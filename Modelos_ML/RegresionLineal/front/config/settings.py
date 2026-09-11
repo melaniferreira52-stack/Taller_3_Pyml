5685623feb72d6ee7b1ad30e464e5f84fcf887fc
Melani Ferreira <melaniferreira52@gmail.com>
2026-09-10 22:20:48 -0500

    Organizar el CSS

--- Modelos_ML/RegresionLineal/front/config/settings.py
+++ Modelos_ML/RegresionLineal/front/config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,3 +131,5 @@ MAILERS = {
 
 import os
 API_URL = os.environ.get('API_URL', 'http://127.0.0.1:8000')
+STATIC_ROOT = BASE_DIR /'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
